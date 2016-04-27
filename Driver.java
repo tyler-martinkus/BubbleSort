641d27f3ec405428db1fcbc748739cddfb6184a6
tyler-martinkus <tyler_martinkus@msn.com>
2016-04-27 09:23:35 -0500

    Inintial Commit

---
+++ Driver.java
@@ -0,0 +1,24 @@
+public class Driver 
+{
+	public static void BubbleSort( int [ ] num )
+	{
+	     int j;
+	     boolean flag = true;  
+	     int temp;   
+
+	     while ( flag )
+	     {
+	            flag= false;    
+	            for( j=0;  j < num.length -1;  j++ )
+	            {
+	                   if ( num[ j ] < num[j+1] )   
+	                   {
+	                           temp = num[ j ];                
+	                           num[ j ] = num[ j+1 ];
+	                           num[ j+1 ] = temp;
+	                          flag = true;              
+	                  } 
+	            } 
+	     }
+	}
+}
